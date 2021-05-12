0643c27b4852733e974d97ec4ca257116585f048
lenarddome <lenarddome@protonmail.ch>
2021-05-12 08:38:09 +0100

    make test units single core

--- tests/testthat/test-polytope.R
+++ tests/testthat/test-polytope.R
@@ -1,3 +1,10 @@
+# set seed
+set.seed(7624)
+# region centres
+positions <- NULL
+for (i in seq_len(5)) positions <- cbind(positions, sample(500, 100))
+
+
 #' dummy model using polytope to test the PSP function
 #' The model takes in a set of coordinates, calculates its distance from all
 #' all of available coordinates, then return closest region number
@@ -7,10 +14,6 @@
 #' @examples
 #' model(runif(5))
 model <- function(par) {
-    set.seed(7624)
-    # region centres
-    positions <- NULL
-    for (i in seq_len(5)) positions <- cbind(positions, sample(500, 100))
     areas <- NULL
     for (i in seq_along(par)) {
         range <- c(1, 0)
@@ -26,12 +29,13 @@ model <- function(par) {
 }
 
 # run Parameter Space Partitioning
-out <- psp_global(model, psp_control(lower = rep(0, 5),
-                                     upper = rep(1, 5),
-                                     init = rep(0.5, 5),
-                                     radius = rep(0.25, 5),
-                                     pop = 200,
-                                     iterations = 250))
+out <- psp_global(model, psp_control(lower = rep(0, 4),
+                                     upper = rep(1, 4),
+                                     init = rep(0.5, 4),
+                                     radius = rep(0.25, 4),
+                                     pop = 300,
+                                     parallel = FALSE,
+                                     iterations = 300))
 
 test_that("PSP finds all 100 regions in a 5 parameter model",
           {expect_equal(length(out[[2]]), 100)
