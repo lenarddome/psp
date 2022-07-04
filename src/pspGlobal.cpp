d37b47270b4eb780bed8aaf3507c9b76891e7384
lenarddome <lenarddome@protonmail.ch>
2022-07-04 16:23:40 +0100

    work on sampling, csv writing, and setting up environemnt for psp

--- src/pspGlobal.cpp
+++ src/pspGlobal.cpp
@@ -1,5 +1,7 @@
+// Copyright 2022 <Lenard Dome> [legal/copyright]
 // [[Rcpp::depends(RcppArmadillo)]]
 #include <RcppArmadillo.h>
+#include <fstream>
 
 // [[Rcpp::depends(RcppArmadillo)]]
 using namespace Rcpp;
@@ -8,75 +10,139 @@ using namespace arma;
 // utility functions
 // Weisstein, Eric W. "Hypersphere Point Picking." From MathWorld.
 // https://mathworld.wolfram.com/HyperspherePointPicking.html
-// pick new jumping distributions from within the unit hypersphere scaled by the radius
-mat .HyperPoints(mat jumping_distribution, colvec radius) {
+// pick new jumping distributions from the unit hypersphere scaled by the radius
+mat .HyperPoints(int counts, int dimensions, colvec radius) {
+  mat hypersphere;
+  // create a uniform distribution
+  hypersphere.randu(counts, dimensions);
+  colvec denominator = sum(hypershphere, 1);
+  denominator = 1 / denominator;
+  // pick points from within unite hypersphere
+  hypersphere = hypersphere.each_row() % denominator;
+  // scale up values by r
+  hypersphere = hypersphere.each_row() % radius;
+  return(hypersphere);
 }
 
 // constrain new jumping distributions within given parameter bounds
-mat .ClampParameters() {
+mat .ClampParameters(mat jumping_distribution, colvec upper, colvec lower) {
+  jumping_distribution.clamp()
 }
 
 // compare two inequality matrices
-vec .OrdinalCompare() {
+// returns TRUE if they are the same or FALSE if they differ in any aspect
+rowvec .OrdinalCompare(mat discovered, mat predicted) {
+  int stimuli = discovered.n_rows;
+  // TODO(LD): boolean matrix
 }
 
 // store ordinal patterns in lists
-List .OrdinalStorage() {
+// takes existing list and appends new ordinal patterns
+List .OrdinalStorage(List ordinal, List new) {
 }
 
 // count ordinal patterns
-vec .CountOrdinal() {
+// if ordinal pattern is output TRUE from .OrdinalStorage
+// add 1 to its count
+rowvec .CountOrdinal(List ordinal, vec counts) {
 }
 
-// pspControl does a bunch of error catching and manipulations to set up
-// the environment for pspGlobal.
-// [[Rcpp::export]]
-List pspControl(List control) {
-
+// writes rows to csv file
+void .WriteFile(int iteration, mat evaluation, int dimension,
+  std::string path_to_file) {
+  // open file stream connection
+  std::ofstream outFile(path_to_file.c_str());
+  int rows = evaluation.n_rows;
+  int columns = dimension + 1;
+  for (size_t i = 0; i < rows; i++) {
+    outFile << i + ",";
+    for (size_t k = 0; k < columns; k++) {
+      outFile << evaluation[i, k] + ",";
+    }
+    outFile << "\n";
+  }
+  // close file connection
+  outFile.close();
 }
 
 // [[Rcpp::export]]
 List pspGlobal(std::string fn, List control, std::string filename,
-               std::string path, bool load = false, bool quiet = false) {
-
+               std::string path, bool quiet = false) {
   // call the ordinal function used for evaluation parameters
-  // TODO: error catching is important here
   try {
     Environment env = Environment::global_env();
     Function model = env[fn];
-  } 
+  }
 
   catch (...) {
-    Rcout << "ERROR: ordinal function fn need to be loaded into global_env" << std::endl;
+    Rcout << "ERROR: ordinal function " << fn << " need to be loaded into global_env" << std::endl;
   }
 
   // setup environment
-  List control_setup = pspControl(control);
 
   bool parameter_filled = false;
   int iteration = 0;
-  int max_iteration = as<int>(setup["iterations"]); 
+
+  int max_iteration = as<int>(control["iterations"]);
+  if (max_iteration == NULL) {
+    max_iteration = datum::inf;
+  }
+
+  int population = as<int>(control["population"]);
+  if (population == NULL) {
+    population =  datum::inf;
+  }
+  colvec radius  = as<colvec>(control["radius"]);
+  colvec  init = as<colvec>(control["init"]);
+  colvec lower = as<colvec>(control["lower"]);
+  colvec upper = as<colvec>(control["upper"]);
+  int dimension = init.size();
+  mat output;
+  rowvec counts;
+  List ordinal;
+  List storage;
+
+  CharacterVector names = as<CharacterVector>(control["param_names"]);
+
+  List out;
+
+  // setup file and create headers
+  std::ofstream outFile(path + filename);
+  outFile << "iteration,";
+  for (size_t i = 0; i < dimensions; i++) {
+    outFile << names[i] + ",";
+  }
+  outFile << names + ",pattern\n";
+  // close file connection
+  outFile.close();
 
   // evaluate first parameter set
+  mat output = model(init);
+  // add output to storage
+  List storage = output;
 
-  while(parameter_filled) {
-
+  while (parameter_filled) {
     // update iteration
     iteration += 1;
 
-    // generate new jumping distributions
+    // generate new jumping distributions from ordinal patterns with counts < population
     // evaluate jumping distributions
     // compare ordinal patterns to stored ones
     // update list of ordinal patterns
     // update counts of ordinal patterns
     // write data to disk
-    // TODO: do this according to https://www.gormanalysis.com/blog/reading-and-writing-csv-files-with-cpp/
-    
+    outFile << "\n";
+
     // print information about iteration
     if (!quiet) {
       Rcout << "Iteration:" << iteration << std::endl;
     }
   }
+
+
+  out = Rcpp::List::create(
+    Rcpp::Named("ordinal_counts") = counts,
+    Rcpp::Named("ordinal_patterns") = storage);
 
   // compile output including ordinal patterns and their frequencies
   return(out)
