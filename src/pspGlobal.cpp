5a594c7eecf1f4252488a843e82d402c59e0d0ad
lenarddome <lenarddome@protonmail.ch>
2022-07-06 11:29:40 +0100

    workaround rcpparmadillo bug in OrdinalCompare, start work on counts

--- src/pspGlobal.cpp
+++ src/pspGlobal.cpp
@@ -34,25 +34,43 @@ mat ClampParameters(mat jumping_distribution, colvec lower, colvec upper) {
 
 // compare two cubes of inequality matrices
 // returns the complete list of unique ordinal matrices
-// [[Rcpp::export]]
 cube OrdinalCompare(cube discovered, cube predicted) {
-  cube ordinal(discovered);
+  cube drawer(discovered);
+  mat index(predicted.n_slices, discovered.n_slices);
   // carry out the comparisons
   for (int x = 0; x < predicted.n_slices; x++) {
     mat current = predicted.slice(x);
-    for (int y = 0; y < discovered.size(); y++) {
-      if (current != discovered.slice(y)) {
-        ordinal.insert_slices(ordinal.n_slices, current);
-      }
+    for (int y = 0; y < discovered.n_slices; y++) {
+      mat base = discovered.slice(y);
+      umat result = (base == current);
+      index(x, y) =  all(any(result == 0));
+    }
+    if (all(index.row(x) == 1)) {
+      cube update = join_slices(drawer, current);
+      drawer = update;
     }
   }
-  return(ordinal);
+  return(drawer);
+}
+
+// find the location of  inequality matrices in storage that correspond
+// to the jumping distributions
+// returns the last evaluated parameters in all chains in the MCMC
+uvec OrdinalMatch(cube discovered, cube predicted, mat jumping, mat centers) {
 }
 
 // count ordinal patterns
-// if ordinal pattern is output TRUE from .OrdinalStorage
-// add 1 to its count
-rowvec CountOrdinal(cube discovered, cube predicted, vec counts) {
+rowvec CountOrdinal(cube updated_ordinal, cube predicted, rowvec counts) {
+  rowvec new_counts = counts;
+  new_counts.resize(updated_ordinal.n_slices)
+  for (int x = 0; x < updated_ordinal.n_slices; x++) {
+    for (int y = 0; y < predicted.size(); y++) {
+      if (current == discovered.predicted(y)) {
+        new_counts[x] += 1;
+      }
+    }
+  }
+  return(new_counts);
 }
 
 // writes rows to csv file
@@ -75,7 +93,7 @@ void WriteFile(int iteration, mat evaluation, int dimension,
 
 // [[Rcpp::export]]
 List pspGlobal(std::string fn, List control, std::string filename,
-               std::string path, bool quiet = false) {
+               std::string path = ".", bool quiet = false) {
   // call the ordinal function used for evaluation parameters
   try {
     Environment env = Environment::global_env();
@@ -137,11 +155,12 @@ List pspGlobal(std::string fn, List control, std::string filename,
 
   // evaluate first parameter set
   mat output = model(init);
-  ordinal.insert_slices(0, output);
+  int stimuli = output.n_rows;
   // add output to storage
   storage.insert_slices(output);
   delete[] output;
 
+  // run parameter space partitioning until parameter is filled
   while (parameter_filled) {
     // update iteration
     iteration += 1;
@@ -150,23 +169,26 @@ List pspGlobal(std::string fn, List control, std::string filename,
     jumping_distribution = HyperPoints(jumping_distribution.n_rows, dimensions, radius);
     jumping_distribution = ClampParameters(jumping_distribution, lower, upper);
 
+    cube ordinal(stimuli, stimuli, jumping_distribution.n_rows);
     // evaluate jumping distributions
     for (uword i = 0; i < jumping_distribution.n_rows; i++) {
       mat evaluate = model(jumping_distribution.row(i));
       ordinal.slice(i) = evaluate;
     }
-    // compare ordinal patterns to stored ones
-    // update list of ordinal patterns
-    if (!new) {
-      /* code here */
-    }
+    // compare ordinal patterns to stored ones and update list
+    storage = OrdinalCompare(storage, ordinal);
     // update counts of ordinal patterns
+    counts = CountOrdinal(storage, ordinal, counts);
     // write data to disk
     outFile << "\n";
 
     // print information about iteration
     if (!quiet) {
       Rcout << "Iteration:" << iteration << std::endl;
+    }
+    // check if parameter_filled threshold is reached
+    if (iteration == threshold || all(counts > population)) {
+      parameter_filled = TRUE
     }
   }
 
