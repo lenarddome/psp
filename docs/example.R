77068cb7fdd00e59e43f03f3b3a77c27922be86f
lenarddome <lenarddome@protonmail.ch>
2022-07-11 17:10:37 +0100

    :art: benchmark tweaks

--- docs/example.R
+++ docs/example.R
@@ -1,33 +1,41 @@
 library(psp)
+library(ggplot2)
 library(data.table)
 library(plyr)
 library(microbenchmark)
+library(Rcpp)
+sourceCpp("../src/pspGlobal.cpp")
 
 # euclidean distance
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
 
 # define center points for the 10 regions in a two-dimensional space
 positions <- NULL
-for (i in seq_len(2)) positions <- cbind(positions, sample(500, 4) / 500)
+for (i in seq_len(2)) positions <- cbind(positions, sample(500, 100))
 
 ## calculates distances and gives a non-sensical inequality matrix
 model <-  function(par, legacy = FALSE) {
-  distances <- outer(as.matrix(par), as.matrix(positions), Vectorize(euclidean))
+  areas <- NULL
+  for (i in seq_along(par)) {
+      range <- c(1, 0)
+      if (i %% 2 == 0) {
+          range <- c(0, 1)
+      }
+      areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
+  }
+  distances <- outer(as.matrix(par), as.matrix(areas), Vectorize(euclidean))
   converted <- plyr::alply(distances, 3)
   summed <- unlist(plyr::alply(distances, 3, sum, expand = FALSE))
-  distance_matrix <- dist(summed, diag = TRUE)
-  distance_matrix[distance_matrix < 0.5] <- -1
-  distance_matrix[distance_matrix < 1.0 & distance_matrix > 0.5] <- 0
-  distance_matrix[distance_matrix > 1.0] <- 1
+  out <- converted[[which.min(summed)]]
   if (legacy) {
-    distance_matrix <- data.frame(as.matrix(distance_matrix))
+    out <- paste(positions[which.min(summed),], collapse = "")
   } else {
-    distance_matrix <- as.matrix(distance_matrix)
+    out <- matrix(which.min(summed), nrow = 2, ncol = 2)
   }
-  distance_matrix[!upper.tri(distance_matrix)] <- NA
-  return(distance_matrix)
+  return(out)
 }
 
+set.seed(1)
 rold <- function() {
   out <- psp_global(model, psp_control(lower = rep(0, 2),
                                      upper = rep(1, 2),
@@ -42,8 +50,8 @@ cpp <- function() {
   outcpp <- pspGlobal(model, control = list(lower = rep(0, 2),
                                    upper = rep(1, 2),
                                    init = matrix(rep(0.5, 2), nrow = 1),
-                                   radius = rep(0.10, 2),
-                                   population = 1000000,
+                                   radius = 0.10,
+                                   population = 2147483647,
                                    param_names = paste("names", 1:2, sep = ""),
                                    iterations = 100),
                  save = TRUE, path = "./benchmark.csv")
@@ -58,3 +66,11 @@ cpp_benchmark <- benchPress()
 
 graph <- ggplot2::autoplot(cpp_benchmark)
 ggsave(plot = graph, filename = "benchmark.pdf", units = in, width = 10, height = 12)
+
+
+test <- fread("benchmark.csv")
+ggplot(test, aes(x = names1, y = names2, color = pattern)) +
+  geom_point()
+
+ggplot(out$ps_partitions, aes(x = parameter_1, y = parameter_2, color = pattern)) +
+  geom_point()
