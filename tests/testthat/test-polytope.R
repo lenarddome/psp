c7ba78a8e5c16b79540a4d64e009fb41dbe41014
lenarddome <lenarddome@protonmail.ch>
2022-08-15 16:41:35 +0100

    :white_check_mark: add test units for cpp

--- tests/testthat/test-polytope.R
+++ tests/testthat/test-polytope.R
@@ -1,42 +1,56 @@
+euclidean <- function(a, b) sqrt(sum((a - b)^2))
+
+# define center points for the 10 regions in a two-dimensional space
+positions <- NULL
 # set seed
 set.seed(7624)
-# region centres
-positions <- NULL
 for (i in seq_len(5)) positions <- cbind(positions, sample(500, 100))
 
+## calculates distances and gives a non-sensical inequality matrix
+model <-  function(par, legacy = FALSE) {
+  areas <- NULL
+  for (i in seq_along(par)) {
+      range <- c(1, 0)
+      if (i %% 2 == 0) {
+          range <- c(0, 1)
+      }
+      areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
+  }
+  distances <- apply(areas, 1, euclidean, b = par)
 
-#' dummy model using polytope to test the PSP function
-#' The model takes in a set of coordinates, calculates its distance from all
-#' all of available coordinates, then return closest region number
-#'
-#' @param x a vector of coordinates
-#' @return The number of the region as character
-#' @examples
-#' model(runif(5))
-model <- function(par) {
-    areas <- NULL
-    for (i in seq_along(par)) {
-        range <- c(1, 0)
-        if (i %% 2 == 0) {
-            range <- c(0, 1)
-        }
-        areas <- cbind(areas,
-                       seq(range[1], range[2],
-                           length.out = 500)[positions[, i]])
-    }
-    dist <- apply(areas, 1, function(x) sqrt(sum((par - x)^2)))
-    return(as.character(which.min(dist)))
+  if (legacy) {
+    out <- as.character(which.min(distances))
+  } else {
+    out <- matrix(which.min(distances), nrow = 2, ncol = 2)
+  }
+  return(out)
 }
 
 # run Parameter Space Partitioning
-out <- psp_global(model, psp_control(lower = rep(0, 5),
+out <- suppressWarnings(
+        psp_global(model, psp_control(lower = rep(0, 5),
                                      upper = rep(1, 5),
                                      init = rep(0.5, 5),
-                                     radius = rep(0.25, 5),
+                                     radius = rep(0.5, 5),
                                      pop = 300,
                                      parallel = FALSE,
-                                     iterations = 300))
+                                     iterations = 100), legacy = TRUE)
+        )
 
 test_that("PSP finds all 100 regions in a 5 parameter model", {
               expect_equal(length(out[[2]]), 100)
+          })
+
+out2 <- pspGlobal(model = model,
+                 control = list(iterations = 100,
+                                population = 300,
+                                radius = 1,
+                                lower = rep(0, 5),
+                                upper = rep(1, 5),
+                                init = rep(0.5, 5),
+                                param_names = c('a', 'd', 'z', 'y', 'x'),
+                                quiet = FALSE))
+
+test_that("PSP finds all 100 regions in a 5 parameter model", {
+              expect_equal(dim(out2$ordinal_pattern)[3], 100)
           })
