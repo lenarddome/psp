84d0b19482a6b56033ec52a278248e064dd5d3c5
lenarddome <lenarddome@gmail.com>
2026-01-15 09:59:06 +0100

    fix: remove deprecated context() calls in testthat cause warnings

--- tests/testthat/test_psp_R.R
+++ tests/testthat/test_psp_R.R
@@ -1,4 +1,3 @@
-context('psp_global')
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
 
 # define center points for the 10 regions in a two-dimensional space
@@ -8,14 +7,14 @@ set.seed(7624)
 for (i in seq_len(5)) positions <- cbind(positions, sample(500, 100))
 
 ## calculates distances and gives a non-sensical inequality matrix
-model <-  function(par, legacy = FALSE) {
+model <- function(par, legacy = FALSE) {
   areas <- NULL
   for (i in seq_along(par)) {
-      range <- c(1, 0)
-      if (i %% 2 == 0) {
-          range <- c(0, 1)
-      }
-      areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
+    range <- c(1, 0)
+    if (i %% 2 == 0) {
+      range <- c(0, 1)
+    }
+    areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
   }
   distances <- apply(areas, 1, euclidean, b = par)
 
@@ -29,15 +28,17 @@ model <-  function(par, legacy = FALSE) {
 
 # run Parameter Space Partitioning
 out <- suppressWarnings(
-        psp_global(model, psp_control(lower = rep(0, 5),
-                                     upper = rep(1, 5),
-                                     init = rep(0.5, 5),
-                                     radius = rep(0.5, 5),
-                                     pop = 300,
-                                     parallel = FALSE,
-                                     iterations = 100), legacy = TRUE)
-        )
+  psp_global(model, psp_control(
+    lower = rep(0, 5),
+    upper = rep(1, 5),
+    init = rep(0.5, 5),
+    radius = rep(0.5, 5),
+    pop = 300,
+    parallel = FALSE,
+    iterations = 100
+  ), legacy = TRUE)
+)
 
 test_that("PSP finds all 100 regions in a 5 parameter model", {
-              expect_equal(length(out[[2]]), 100)
-          })
+  expect_equal(length(out[[2]]), 100)
+})
--- tests/testthat/test_psp_cpp.R
+++ tests/testthat/test_psp_cpp.R
@@ -1,4 +1,3 @@
-context('pspGlobal')
 euclidean <- function(a, b) sqrt(sum((a - b)^2))
 
 # define center points for the 10 regions in a two-dimensional space
@@ -8,14 +7,14 @@ set.seed(7624)
 for (i in seq_len(5)) positions <- cbind(positions, sample(500, 100))
 
 ## calculates distances and gives a non-sensical inequality matrix
-model <-  function(par) {
+model <- function(par) {
   areas <- NULL
   for (i in seq_along(par)) {
-      range <- c(1, 0)
-      if (i %% 2 == 0) {
-          range <- c(0, 1)
-      }
-      areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
+    range <- c(1, 0)
+    if (i %% 2 == 0) {
+      range <- c(0, 1)
+    }
+    areas <- cbind(areas, seq(range[1], range[2], length.out = 500)[positions[, i]])
   }
   distances <- apply(areas, 1, euclidean, b = par)
   return(distances)
@@ -30,55 +29,59 @@ discretize <- function(distances, legacy = FALSE) {
   return(out)
 }
 
-out2 <- pspGlobal(model = model, discretize = discretize,
-                 control = list(iterations = 1000,
-                                population = 10,
-                                radius = 1,
-                                lower = rep(0, 5),
-                                upper = rep(1, 5),
-                                init = matrix(rep(seq(0, 1, length.out = 10), 5), nrow = 10, byrow = TRUE),
-                                parameter_names = c('a', 'd', 'z', 'y', 'x'),
-                                stimuli_names = as.character(seq(100)),
-                                dimensionality = 2,
-                                responses = 100),
-                                quiet = FALSE, save = FALSE)
+out2 <- pspGlobal(
+  model = model, discretize = discretize,
+  control = list(
+    iterations = 1000,
+    population = 10,
+    radius = 1,
+    lower = rep(0, 5),
+    upper = rep(1, 5),
+    init = matrix(rep(seq(0, 1, length.out = 10), 5), nrow = 10, byrow = TRUE),
+    parameter_names = c("a", "d", "z", "y", "x"),
+    stimuli_names = as.character(seq(100)),
+    dimensionality = 2,
+    responses = 100
+  ),
+  quiet = FALSE, save = FALSE
+)
 
 test_that("PSP finds all 100 regions in a 5 parameter model", {
-              expect_equal(dim(out2$ordinal_pattern)[3], 100)
-          })
+  expect_equal(dim(out2$ordinal_pattern)[3], 100)
+})
 
 test_that("PSP population terminates algorithm before iteration cap reached", {
   expect_true(out2$iterations < 1000)
-  }
-)
+})
 
 test_that("Part 1: Unique unequality matrices only recruited once", {
   expect_true(all(!(table(out2$ordinal_patterns) > 4)))
-  }
-)
+})
 
-foo <- lapply(seq(dim(out2$ordinal_patterns)[3]), function(x) out2$ordinal_patterns[ , , x])
+foo <- lapply(seq(dim(out2$ordinal_patterns)[3]), function(x) out2$ordinal_patterns[, , x])
 
 test_that("Part 2: PSP only recruits items once", {
-    expect_equal(dim(out2$ordinal_patterns)[3], length(foo))
-  }
+  expect_equal(dim(out2$ordinal_patterns)[3], length(foo))
+})
+
+
+out3 <- pspGlobal(
+  model = model, discretize = discretize,
+  control = list(
+    iterations = 100,
+    population = 30000,
+    radius = 1,
+    lower = rep(0, 5),
+    upper = rep(1, 5),
+    init = matrix(rep(seq(0, 1, length.out = 10), 5), nrow = 10, byrow = TRUE),
+    parameter_names = c("a", "d", "z", "y", "x"),
+    stimuli_names = as.character(seq(100)),
+    dimensionality = 2,
+    responses = 100
+  ),
+  quiet = FALSE, save = FALSE
 )
-
-
-out3 <- pspGlobal(model = model, discretize = discretize,
-                 control = list(iterations = 100,
-                                population = 30000,
-                                radius = 1,
-                                lower = rep(0, 5),
-                                upper = rep(1, 5),
-                                init = matrix(rep(seq(0, 1, length.out = 10), 5), nrow = 10, byrow = TRUE),
-                                parameter_names = c('a', 'd', 'z', 'y', 'x'),
-                                stimuli_names = as.character(seq(100)),
-                                dimensionality = 2,
-                                responses = 100),
-                                quiet = FALSE, save = FALSE)
 
 test_that("PSP iteration threshold terminates algorithm succesfully", {
   expect_true(out3$iterations == 100)
-  }
-)
+})
